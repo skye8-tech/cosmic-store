9557b54b82f3d61d7e02e97c221856b92276067c
Spyke Lionel <social.lionel@gmail.com>
2024-08-06 23:55:32 +0100

    adjust imports

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
+import { cartApi, productApi } from "@app/services";
 import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
   reducer: {
     [cartApi.reducerPath]: cartApi.reducer,
+    [productApi.reducerPath]: productApi.reducer,
   },
-  middleware: (middlewares) => [middlewares.concat(cartApi.middleware)],
+  middleware: (middlewares) => [
+    middlewares.concat(cartApi.middleware).concat(productApi.middleware),
+  ],
 });
