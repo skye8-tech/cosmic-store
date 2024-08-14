90b4a94c4ea5cfd65fc58c1a50f4b401de74c238
N-TITIAN <ndifon.titian@gmail.com>
2024-08-14 19:06:04 +0100

    LOGIN

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ function Button(props) {
 const {variant, text, width, icon, href, funct }=props;
 
 const buttonClasses=`
- ${variant === 'primary' ? 'bg-secondary2 font-bold  text-white py-2 flex flex-row justify-center items-center gap-4 rounded-lg hover:bg-black hover:text-secondary2' :'bg-none font-normal text-black border border-black py-2 flex flex-row justify-center items-center gap-4 rounded-lg hover:bg-secondary2 hover:text-white' }
+ ${variant === 'primary' ? 'bg-secondary2 font-bold  text-white py-2 px-4 flex flex-row justify-center items-center gap-4 rounded-lg hover:bg-black hover:text-secondary2' :'bg-none font-normal text-black border border-black py-2 px-4 flex flex-row justify-center items-center gap-4 rounded-lg hover:bg-secondary2 hover:text-white' }
  ${width === 'fit' ? 'w-fit' : width === 'full' ? 'w-full' : ''}
 `
 return(
--- src/pages/Authentication/components/LoginForm.jsx
+++ src/pages/Authentication/components/LoginForm.jsx
@@ -1,47 +1,38 @@
 import Button from '@components/Button'
 import React from 'react'
 
-function RegistrationForm() {
+function LoginForm() {
   return (
-    <>
+   <>
     <form action="POST" className='flex flex-col gap-8 w-full p-8 lg:p-0'>
         <div className='flex flex-col gap-4'>
-        <p className='text-4xl font-medium'>Create an account</p>
+        <p className='text-4xl font-medium'>Log in to Exclusive</p>
         <p className='font-medium'> Enter your details below </p>
         </div>
         <div className='flex flex-col gap-4'>
-        <input type="text" placeholder='Name' className='p-2 text-sm  border-b-2 border-gray-400 w-full'/>
         <input type="text " placeholder='Email or Phone Number ' className='p-2 text-sm border-b-2 border-gray-400 w-full'/>
         <input type="text" placeholder='Password' className='p-2  border-b-2 text-sm border-gray-400 w-full'/>
         
         </div>
 
         
-        <div className='flex flex-col gap-4'>
+        <div className='flex flex-row justify-between'>
         {/* buttons */}
         <Button
-        text="Create Account"
+        text="Login "
         variant="primary"
-        width="full"
+        width="fit"
         href="#"
         funct=""
         icon=""
 
         ></Button>
-         <Button
-        text="Sign Up With Google"
-        variant="secondary"
-        width="full"
-        href="#"
-        funct=""
-        icon=""
-
-        ></Button>
+       <a href="#" className='text-secondary2 p-2 '> Forgot Password ?</a>
         </div>
-        <p className='font-light text-center'>Already have an account? <span className='font-normal'> <a href="/login">Log in </a></span></p>
+        <p className='font-light text-center'>Don't have an account? <span className='font-normal'> <a href="/register">Sign UP </a></span></p>
     </form>
-    </>
+   </>
   )
 }
 
-export default RegistrationForm
+export default LoginForm
