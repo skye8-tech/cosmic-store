19c3cbe7d59708914def563046bf81e9b1514959
bayeka binet <minetbayeka@gmail.com>
2024-08-21 07:31:04 -0700

    working on it responsiveness

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -9,30 +9,32 @@ import image4 from '../../assets/images/MasterCard.jpg'
 
 const checkout = () => {
   return (
-    <div className='max-w-6xl mx-96'>
+
+    <section className='px-4' >
+      <div className='max-w-6xl  md:mx-auto mx-2'>
         <div className='mt-10'>
             <h1>Account/My Account/ Product/View Cart/<span className='font-bold'>Checkout</span></h1>
         </div>
 
-        <div className='flex justify-between grid-cols-1'>
+        <div className='flex justify-between md:grid-cols-1 md:flex-row flex-col mr-2 sm:mr-auto'>
             <div className=''>
                 <h1 className='font-bold text-4xl mt-10 mb-8'>Billing Details</h1>
 
                 <form className='flex flex-col gap-x-10 gap-4'>
                 <label className='text-xl'>First Name</label>
-                <input type='text'name='first Name' className='bg-[#F5F5F5] w-[35rem] h-10'/>
+                <input type='text'name='first Name' className='bg-[#F5F5F5] md:w-[35rem] w-[33rem]  h-10'/>
                 <label className='text-xl'>Company Name</label>
-                <input type='text'name='Name' className='bg-[#F5F5F5] w-[35rem] h-10'/>
+                <input type='text'name='Name' className='bg-[#F5F5F5] md:w-[35rem] w-[33rem]  h-10'/>
                 <label className='text-xl'>Street Address</label>
-                <input type='text'name='Address' className='bg-[#F5F5F5] w-[35rem] h-10'/>
+                <input type='text'name='Address' className='bg-[#F5F5F5] md:w-[35rem] w-[33rem]  h-10'/>
                 <label className='text-xl'>Appointment, float, etc(Optional)</label>
-                <input type='text'name='' className='bg-[#F5F5F5] w-[35rem] h-10'/>
+                <input type='text'name='' className='bg-[#F5F5F5] md:w-[35rem] w-[33rem]  h-10'/>
                 <label className='text-xl'>Town/City</label>
-                <input type='text'name='' className='bg-[#F5F5F5] w-[35rem] h-10'/>
+                <input type='text'name='' className='bg-[#F5F5F5] md:w-[35rem] w-[33rem]  h-10'/>
                 <label className='text-xl'>Phone Number</label>
-                <input type='text'name='' className='bg-[#F5F5F5] w-[35rem] h-10'/>
+                <input type='text'name='' className='bg-[#F5F5F5] md:w-[35rem] w-[33rem]  h-10'/>
                 <label className='text-xl'>Email Address</label>
-                <input type='text'name='Email' className='bg-[#F5F5F5] w-[35rem] h-10'/>
+                <input type='text'name='Email' className='bg-[#F5F5F5] md:w-[35rem] w-[33rem] h-10'/>
                 <div className='flex items-center gap-4 mt-4 mb-20'>
                 <input type='checkbox' value="" className='bg-black'/>
                 <label className='text-xl'>Save this information for faster check-out next time</label>
@@ -41,9 +43,11 @@ const checkout = () => {
 
                 </form>
             </div>
+
+            
             {/**right side */}
 
-            <div className='mt-36'>
+            <div className='md:mt-36'>
               <div className='flex justify-between gap-16'>
                 <div className='flex gap-x-8 items-center'>
                  <div>
@@ -114,15 +118,13 @@ const checkout = () => {
                 <label className='text-xl'>Cash on delivery</label>
                 </div>
 
-              {/* <div className='flex justify-between mt-8'>
-                <p className='text-xl'>Shipping:</p>
-                <p className='text-xl'>Free</p>
-              </div> */}
+              
 
               <div className='flex flex-wrap  gap-6 mt-12'>
-                <button className='border w-60 '>
+                 <button className='border w-80 hover:text-white hover:bg-secondary2 '>
                   Coupon Code
-                </button>
+                </button> 
+      
 
               <div className=''>
                 <Button
@@ -131,19 +133,21 @@ const checkout = () => {
                 
                 /></div>
 
-               <Button
-                variant ="primary"
-                text="Place Order"
-                
-                />
-
               </div>
+              <div className='mt-4 md:mb-0 mb-4'>
+              <Button
+               
+               variant ="primary"
+               text="Place Order"
+               
+               /></div>
 
             </div>
             
         </div>
+        </div>
       
-    </div>
+    </section>
   )
 }
 
