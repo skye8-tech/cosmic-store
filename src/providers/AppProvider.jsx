b273b4db2b8e8fde5e777d5e1fc9cc4bf4117fce
Spyke Lionel <social.lionel@gmail.com>
2024-08-28 15:24:43 +0100

    context api

--- src/providers/AppProvider.jsx
+++ src/providers/AppProvider.jsx
@@ -1,11 +1,41 @@
 import { store } from "@app";
+import { AppContext } from "@providers";
 import { router } from "@router";
+import { useState } from "react";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+
+// export const AppContext = createContext();
+const ContextProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const logOut = () => {
+    console.log("User is loggin out.....");
+    setIsLoggedIn(false);
+  };
+  const logIn = () => {
+    console.log("User is loggin out.....");
+    setIsLoggedIn(true);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        logOut: logOut,
+        logIn: logIn,
+        // other context values here...
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 
 export const AppProvider = () => (
   <Provider store={store}>
     {/* Other providers */}
-    <RouterProvider router={router} />
+    {/* AppContext */}
+    <ContextProvider>
+      <RouterProvider router={router} />
+    </ContextProvider>
   </Provider>
 );
