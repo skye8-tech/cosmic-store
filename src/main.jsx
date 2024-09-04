3642b7a4b969ea63a77ff2a77bb2d2672de74cb9
N-TITIAN <ndifon.titian@gmail.com>
2024-09-04 19:17:38 +0100

    rethink

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import "@assets/styles/index.css";
 import { AppProvider } from "@providers";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useAppContext } from "@hooks/useAppContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppProvider />
+    {/* <AppProvider /> */}
+    const {(isLoggedIn, logOut, logIn)} = useAppContext();
   </React.StrictMode>
 );
